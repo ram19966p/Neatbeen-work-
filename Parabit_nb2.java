2e8a068b704597805a47072c7be3f7135566c269
Ramlakhan patel <138600057+ram19966p@users.noreply.github.com>
2024-05-19 10:55:07 +0530

    "Sum of two number in using Neatbean"
    I am Using in first time in Neatbean

---
+++ Parabit_nb2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parabit_nb2;
+
+/**
+ *
+ * @author ramla
+ */
+public class Parabit_nb2 {
+
+    public static void main(String[] args) {
+          int a=10 ;
+          int b=50;
+           int c;     
+           c=a+b;
+        System.out.println("Sum of two number is "+c);
+         
+    }
+}
