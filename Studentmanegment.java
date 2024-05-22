7f4dad50470a3a53d64d5058a89b9d8eb32147d5
Ramlakhan patel <138600057+ram19966p@users.noreply.github.com>
2024-05-22 08:39:57 +0530

    Add files via upload

---
+++ Studentmanegment.java
@@ -0,0 +1,26 @@
+
+
+package com.mycompany.studentmanegment;
+
+
+public class Studentmanegment {
+
+ 
+
+    public static void main(String[] args) {
+       StusentDB  obj;
+       obj = new StusentDB();
+//      Capture  web1 ;
+//       web1 =  new  Capture();
+  
+       
+     CaptureAndSaveImage   open ;
+      open = new  CaptureAndSaveImage();
+      
+        System.out.println("Hello World!");
+        
+       
+    }
+
+   
+}
