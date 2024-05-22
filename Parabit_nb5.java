2a00a8b68e9c07114c594709d4b32c57e265b55e
Ramlakhan patel <138600057+ram19966p@users.noreply.github.com>
2024-05-22 08:42:43 +0530

    Add files via upload

---
+++ Parabit_nb5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parabit_nb5;
+
+/**
+ *
+ * @author ramla
+ */
+public class Parabit_nb5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
